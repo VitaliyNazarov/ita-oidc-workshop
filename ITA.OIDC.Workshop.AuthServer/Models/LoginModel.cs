425159c01700b59e3e7ead69243a32f610fc44c4
Vitaly Nazarov <vnazarov@ita-labs.ru>
2023-04-24 00:17:08 +0300

    Fix

--- ITA.OIDC.Workshop.AuthServer/Models/LoginModel.cs
+++ ITA.OIDC.Workshop.AuthServer/Models/LoginModel.cs
@@ -6,12 +6,12 @@ namespace ITA.OIDC.Workshop.AuthServer.Models
     {
         [Display(Name = "Логин")]
         [Required(AllowEmptyStrings = false)]
-        public string Login { get; set; }
+        public string Login { get; set; } = string.Empty;
         
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
         [Required(AllowEmptyStrings = false)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
         
         public bool? RememberMe { get; set; }
         
